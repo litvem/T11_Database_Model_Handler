8ef7a6b010048389c632f86b496dc5cbbd9721ce
Robert <guseinero@student.gu.se>
2022-11-18 13:38:56 +0100

    #1 Add schemas for booking-and dentist-model

---
+++ models/dentist.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookingSchema = new Schema({
+  dentistid: { type: Schema.Types.ObjectId, ref: 'dentists'},
+  userid: { type: Number },
+  requestid: { type: Number },
+  dentistid: { type: Number },
+  issuance: { type: Number },
+  date: { type: Date },
+  time: { type: String }
+});
+
+const booking = mongoose.model("bookings", bookingSchema);
+module.exports = booking;
