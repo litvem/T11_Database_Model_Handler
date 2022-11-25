170632dbc567aa9fa7ce01f86a97a3a578a074b7
Robert <guseinero@student.gu.se>
2022-11-25 11:28:01 +0100

    #1 Add required fields for the dentist schema and booking schema, as well as some custom validators for the booking schema

--- src/models/dentist.js
+++ src/models/dentist.js
@@ -3,22 +3,22 @@ const Schema = mongoose.Schema;
 
 // The Dentist schema
 const dentistSchema = new Schema({
-  id: { type: Number },
-  name: { type: String },
-  owner: { type: String },
-  dentists: { type: Number },
-  address: { type: String },
-  city: { type: String },
+  id: { type: Number, required: [true, "Dentist ID is required"] },
+  name: { type: String, required: [true, "Dentist name is required"] },
+  owner: { type: String, required: [true, "Owner of clinic is required"]  },
+  dentists: { type: Number, required: [true, "Number of dentists is required"]  },
+  address: { type: String, required: [true, "Address is required"] },
+  city: { type: String, required: [true, "City is required"] },
   coordinate: {
-    longitude: { type: Number },
-    latitude: { type: Number },
+    longitude: { type: Number,  required: [true, "Longitude is required"] },
+    latitude: { type: Number, required: [true, "Latitude is required"] },
   },
   openinghours: {
-    monday: { type: String },
-    tuesday: { type: String },
-    wednesday: { type: String },
-    thursday: { type: String },
-    friday: { type: String },
+    monday: { type: String, required: [true, "Monday opening hours is required"] },
+    tuesday: { type: String, required: [true, "Tuesday opening hours is required"] },
+    wednesday: { type: String, required: [true, "Wednesday opening hours is required"] },
+    thursday: { type: String, required: [true, "Thursday opening hours is required"]},
+    friday: { type: String, required: [true, "Friday opening hours is required"] },
   },
 });
 
