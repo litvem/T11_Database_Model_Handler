6cba52cf353fe29a1f4f0c88a827fbc268ba59dc
Robert <guseinero@student.gu.se>
2022-11-25 11:26:42 +0100

    #1 Add  MQTT on-message function listening to requests to publish dentist clinic data and add QoS 1 for for pub/sub methods

--- src/communication/mqtt.js
+++ src/communication/mqtt.js
@@ -2,33 +2,48 @@ const mqtt = require("mqtt");
 const client = mqtt.connect("mqtt://localhost:1883");
 const dentist = require("../models/dentist");
 const booking = require("../models/booking");
+const options = {
+  qos: 1
+}
 
-const pubTopicList = {
+
+const pub_topics_list = {
   dataDentistResponse: "data/dentist/response",
   bookingConfirmed: "booking/confirmed/",
   bookingError: "booking/error/",
 };
-const subTopicList = {
+const sub_topics_list = {
   dataDentistRequest: "data/dentist/request",
   saveBooking: "booking/save",
 };
 
 client.on("connect", () => {
   console.log("Connected to the Mosquitto broker");
-  client.subscribe(Object.values(subTopicList), { qos: 1 }, (err) => {
+  client.subscribe(Object.values(sub_topics_list), options, (err) => {
     if (!err) {
-      // console.log(`Subscribed to topics: ${  }`);
+      console.log(
+        `Subscribed to topics: ${sub_topics_list.dataDentistRequest} & ${sub_topics_list.saveBooking}`
+      );
+
     }
   });
 });
+
+client.on("message", (topic, message) => {
+  switch (topic) {
+    case sub_topics_list.dataDentistRequest:
+      findAllDentists();
+      break;
+  }
+});
+  
 
 function findAllDentists() {
   dentist.find({}, (err, dentists) => {
     if (err) {
       console.log(err);
     } else {
-      const dentistsString = JSON.stringify(dentists);
-      client.publish(pubTopicList.dataDentistResponse, dentistsString);
+      client.publish(pub_topics_list.dataDentistResponse, JSON.stringify(dentists), options);
       console.log(dentists);
     }
   });
